8cca1d8eb7610f2b2c5605f95f80702ed988c334
LC1C_22_moriya_yugo <k024g0016@g.neec.ac.jp>
2025-12-19 11:22:49 +0900

    06_02完成

--- project/Object.cpp
+++ project/Object.cpp
@@ -1,19 +1,4 @@
 #include "Object.h"
 
-void Object::Initialize(ObjectCommon* objectCommon) {
-	// 引数で受け取ってメンバ変数に記録する
-	objectCommon_ = objectCommon;
-	// モデル読み込み
-	modelData = LoadObjFile("Resource", "plane.obj");
-
-}
-
-// .mtlファイルの読み込み
-MaterialData Object::LoadMaterialTemplateFile(const std::string& directorypath, const std::string& filename) {
-	return MaterialData();
-}
-
-// .objファイルの読み込み
-ModelData Object::LoadObjFile(const std::string& dirextorypath, const std::string& filename) {
-	return ModelData();
+void Object::Initialize() {
 }
