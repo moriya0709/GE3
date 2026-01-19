dfa1d4f27f81db3109bf180330ca294337f00104
LC1C_22_moriya_yugo <k024g0016@g.neec.ac.jp>
2026-01-19 14:16:16 +0900

    10_01完成

---
+++ project/Game.cpp
@@ -0,0 +1,7 @@
+#include "Game.h"
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+
+
+
+}
---
+++ project/Game.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <Windows.h>
+
+class Game {
+public:
+	// ‰Šú‰»
+	void Initialize();
+	// XV
+	void Update();
+	// •`‰æ
+	void Draw();
+
+	// I—¹
+	void Finalize();
+
+};
+
